f31c451977b74f14b4e2d5a5f1427728c077e169
somavrg <122276670+somavrg@users.noreply.github.com>
2023-08-14 11:23:33 +0200

    Initialize master

---
+++ src/main/java/com/codecool/marsexploration/Application.java
@@ -0,0 +1,10 @@
+package com.codecool.marsexploration;
+
+import java.util.Random;
+
+public class Application {
+    public static void main(String[] args) {
+        Random random = new Random();
+        System.out.println(random.nextInt(42));
+    }
+}
---
+++ src/main/java/com/codecool/marsexploration/data/Coordinate.java
@@ -0,0 +1,4 @@
+package com.codecool.marsexploration.data;
+
+public record Coordinate(int x, int y) {
+}
