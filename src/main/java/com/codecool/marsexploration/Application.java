206aa2f3a0bce19f8ad647a2cd2b7351d426e358
oliver-nagy <olivernagy490@gmail.com>
2023-08-15 17:03:56 +0200

    feat: implement map printer

--- src/main/java/com/codecool/marsexploration/Application.java
+++ src/main/java/com/codecool/marsexploration/Application.java
@@ -1,10 +1,12 @@
 package com.codecool.marsexploration;
 
+import java.util.Map;
 import java.util.Random;
 
 public class Application {
     public static void main(String[] args) {
         Random random = new Random();
         System.out.println(random.nextInt(42));
+
     }
 }
--- src/main/java/com/codecool/marsexploration/MapPrinter.java
+++ src/main/java/com/codecool/marsexploration/MapPrinter.java
@@ -6,13 +6,21 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class MapPrinter {
-    public void print(String filePath, Map<Coordinate, TerrainType> virtualMap) {
+    public void print(String filePath, int mapSize, Map<Coordinate, TerrainType> virtualMap) {
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter(filePath));
-            StringBuilder stringBuilder = new StringBuilder();
-
+            for (int y = 0; y < mapSize; y++) {
+                StringBuilder stringBuilder = new StringBuilder();
+                for (int x = 0; x < mapSize; x++) {
+                    Coordinate coordinate = new Coordinate(x, y);
+                    stringBuilder.append(virtualMap.get(coordinate).symbol());
+                }
+                writer.write(stringBuilder.toString());
+                writer.newLine();
+            }
 
             writer.close();
         } catch (IOException e) {
